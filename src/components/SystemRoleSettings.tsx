3f330b4bb200532da0cd702156323089884292bd
hswmartin <hswmartin@gmail.com>
2023-03-06 09:59:45 +0800

    角色中文化

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -35,7 +35,7 @@ export default (props: Props) => {
         <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
           <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="inline-flex items-center justify-center gap-1 text-sm text-slate bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50">
             <IconEnv />
-            <span>指定角色</span>
+            <span>指定系统角色</span>
           </span>
         </Show>
       </Show>
@@ -43,7 +43,7 @@ export default (props: Props) => {
         <div>
           <div class="flex items-center gap-1 op-60 text-slate">
             <IconEnv />
-            <span>系统系统角色:</span>
+            <span>系统角色:</span>
           </div>
           <p class="my-2 leading-normal text-slate text-sm op-60">让GPT扮演你指定的角色</p>
           <div>
