35e66d0e67ce264cb6ea744ea3b0903984ec55bb
hswmartin <hswmartin@gmail.com>
2023-03-10 22:15:15 +0800

    +send message frequency limit

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -11,13 +11,26 @@ const superKey=import.meta.env.SUPER_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 const sitePassword = import.meta.env.SITE_PASSWORD
-
+let count = 0;
+let timestamp = 0;
 export const post: APIRoute = async (context) => {
+  const now = Date.now();
+  if (now - timestamp < 10000) {
+    count++;
+  } else {
+    count = 1;
+    timestamp = now;
+  }
+  console.log(`Method has been called ${count} times in the last minute.`);
+  
   const body = await context.request.json()
   const { sign, time, messages, pass, key } = body
   let apiKey = import.meta.env.OPENAI_API_KEY
   if(key!=superKey&&key){
     apiKey=key
+  }
+  if(count>=3){
+    return new Response("You have sent too many messages in a short period of time. Please reduce the frequency of sending messages.")
   }
   if (!messages) {
     return new Response('No input text')
