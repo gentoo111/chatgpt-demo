2d776e21befeb4ae6b8cc30cfe6803ca25d8efa3
hswmartin <hswmartin@gmail.com>
2023-03-12 23:53:01 +0800

    test:env

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -130,7 +130,7 @@ export default (props:Props) => {
             const controller = new AbortController()
             setController(controller)
             const originRequestMessageList = [...messageList()]
-            let requestMessageList=originRequestMessageList.slice(-import.meta.env.MSG_LIMIT??3)
+            let requestMessageList=originRequestMessageList.slice(-process.env.MSG_LIMIT??3)
             if (currentSystemRoleSettings()) {
                 requestMessageList.unshift({
                     role: 'system',
